7d44eec72572160b27a1eb7a54d1b34bc4406c8f
Evan Candler <emcandler21@gmail.com>
2017-12-21 06:14:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -101,7 +101,11 @@ def draw?(board)
 end
 
 def over?(board)
-  if (won?(board) || full?(board) || draw?(board))
+  if won?(board) && full?(board) || draw?(board)
+  return true
+elsif won?(board) && !full?(board)
+  return true
+elsif draw?board
   return true
 else
   false
