f31265f81fc74487a9d9f5c5f561692aed2e8a0c
Evan Candler <emcandler21@gmail.com>
2017-12-21 04:01:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -111,9 +111,10 @@ def play
   until over?(board)
     turn
   end
+  if draw?
+    puts "Cat's Game!"
+  end
   if won?
     puts "Congratulations" + winner + "!"
-  elsif draw?
-    puts "Cat's Game!"
   end
 end
