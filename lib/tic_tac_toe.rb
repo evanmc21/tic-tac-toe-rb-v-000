c41312e45de099d9dd7f45bf0b192969bf3b1f80
Evan Candler <emcandler21@gmail.com>
2017-12-21 05:11:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,7 +22,7 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
+def move(board, index, current_player = " ")
   board[index] = current_player
 end
 
